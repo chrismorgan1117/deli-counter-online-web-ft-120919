f6c07e252b07d6f4be67b846d24aa9ae3e6ec15d
Christopher Morgan <cwm111794@gmail.com>
2019-12-12 01:22:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def take_a_number(array, new_person)
   array.push(new_person)
-  puts "Welcome, #{new_person}. You are #{array.length} "
+  puts "Welcome, #{new_person}. You are number#{array.length} in line."
   
   
   
